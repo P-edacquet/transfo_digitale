fa33d42ee25604cf82685062396079ac60137cd3
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-05-30 12:09:58 +0200

    routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  get 'pages/home'
+  get 'pages/map'
+  controller :pages do
+    get :home, to: 'pages#home', as: :accueil
+    get :map, to: 'pages#map', as: :map
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
